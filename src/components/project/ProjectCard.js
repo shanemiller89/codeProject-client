8b2a4415ad18d46b64cffa8266eda76d1a1d1457
Shane Miller <shanemiller89@gmail.com>
2019-10-27 23:17:57 -0500

    list cards complete

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -7,13 +7,24 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import Code from '@material-ui/icons/Code';
+
+
+
+
+import './projects.css'
+import { flexbox } from '@material-ui/system';
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 345,
+    maxWidth: 445,
+
   },
   media: {
-    height: 140,
+    height: 300,
+    objectFit: "scale-down",
+    textAlign: "center"
   },
 });
 
@@ -23,21 +34,30 @@ const ProjectCard = (props) => {
   return (
     <Card className={classes.card}>
       {/* <CardActionArea> */}
-        <CardMedia
+        {/* <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
+          image={props.project.project_image}
+          title={props.project.title}
+        /> */}
+        <div style={{display: "flex", justifyContent: "center"}}>
+        <img src={props.project.project_image} alt={props.project.title} className={classes.media}/>
+        </div>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant="h4" component="h2">
             {props.project.title}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body2" color="textSecondary" component="p" style={{maxHeight: 75, overflow: "hidden", textOverflow: "ellipsis"}}>
               {props.project.overview}
           </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          ...
+          </Typography>
           <br/>
-          <Typography variant="body2"  component="h4">
-              Language:
+          <Typography variant="h5"  component="h4">
+              Language: <Chip color="primary" size="small" label={props.project.technologies[0].technology} icon={<Code />} />
+
+              
+              {/* <span className="primaryLanguage">{props.project.technologies[0].technology}</span> */}
           </Typography>
           
         </CardContent>
