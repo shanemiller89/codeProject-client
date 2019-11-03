5bb1fb2b365420b87db3162016d99923a86ba5aa
Shane Miller <shanemiller89@gmail.com>
2019-11-02 21:39:34 -0500

    collab start

---
+++ src/components/dashboard/DashboardRecentCoallabRequest.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Typography from "@material-ui/core/Typography";
+
+
+const DashboardRecentCollabRequest = () => {
+  return (
+    <React.Fragment>
+      <Typography color="textSecondary" component="h3" variant="h6">
+        Recent Collaboration Request
+      </Typography>
+     
+    </React.Fragment>
+  );
+};
+
+export default DashboardRecentCollabRequest;
---
+++ src/components/dashboard/DashboardRecentTasks.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Typography from "@material-ui/core/Typography";
+
+
+const DashboardRecentTasks = () => {
+  return (
+    <React.Fragment>
+      <Typography color="textSecondary" component="h1" variant="h4">
+        Recent Tasks
+      </Typography>
+      
+     
+    </React.Fragment>
+  );
+};
+
+export default DashboardRecentTasks;
